d8045497f4c7b4c086461993640337ff79ad2e97
Dave Gumba <dave_gumba@Yahoo.com>
2019-08-05 22:01:50 -0400

    first commit, it's up and running though

---
+++ analyze.py
@@ -0,0 +1,17 @@
+import sys
+from collections import Counter
+
+file = open(sys.argv[1] + ".txt", "r")
+frequency_file = open(str(sys.argv[1]) + "_word_frequency.txt", "w")
+punctuation = [',', '.', '.', '..', '...', ';', ':', '?', '\'', '(', ')', '!', '-', '*']
+new_string = ""
+
+for line in file:
+    new_line = ''.join([i for i in line if not i.isdigit() and i not in punctuation])
+    new_line = new_line.lower()
+    new_string += new_line
+    
+word_frequency = Counter(new_string.split()).most_common()
+frequency_file.write('UNIQUE WORD COUNT: ' + str(len(word_frequency)) + '\n')
+for word in word_frequency:
+    frequency_file.write(word[0] + '\t' + str(word[1]) + '\n')
